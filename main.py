ede9a7887a6b054ac6a31661db8f8891cb95db26
smarinv16 <funemesanchez@gmail.com>
2023-05-25 08:37:31 -0500

    se agregan tablas hechas en clase

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from data.data1 import apartamento1, apartamento2
+from helpers.tablasHTML import crearTabla
 import pandas as pd
 
 
@@ -7,14 +8,19 @@ tabla2 = pd.DataFrame(apartamento2,columns=['edad'])
 tabla3 =pd.read_csv("./data/empleados.csv")
 
 
+# Filtrando y aplicando condiciones 
 
-datosEstadisticosApto1 = tabla1.describe()
-datosEstadisticosApto2 = tabla2.describe()
-datosEstadisticosEmpresa = tabla3.describe()
+empleadosJovenes = tabla3.query('edad < 28 and cargo == "analista1"')
+
+analistas1 = tabla3.query('cargo == "analista1"')
+
+analistas2 = tabla3.query('cargo == "analista2"')
+
+empleadosMayores = tabla3.query('edad >= 50')
 
 
-print(datosEstadisticosApto1)
-print("\n")
-print(datosEstadisticosApto2)
-print("\n")
-print(datosEstadisticosEmpresa)
+# Generacion de tablas 
+
+crearTabla(analistas1, "analistas 1")
+crearTabla(analistas2, "analistas 2")
+crearTabla(empleadosMayores, "Jubilados")
